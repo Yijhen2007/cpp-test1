97f0899362b9bc63332157fa5ab0d9f4af4f5ba4
Yijhen2007 <s1120354@dmhs.kh.edu.tw>
2025-10-16 14:27:41 +0800

    hw5

--- test1/main.cpp
+++ test1/main.cpp
@@ -2,9 +2,29 @@
 using namespace std;
 
 int main() {
-    int sum = 0;
-    for (int i = 1; i <= 50; i++) {
-        if ((i % 7) == 0) cout << i << " ";
+    // 輸出星號三角形
+    cout << "=== 星號三角形 ===" << endl;
+    for (int i = 1; i <= 6; ++i) {
+        for (int j = 1; j <= i; ++j) {
+            cout << "*";
+        }
+        cout << endl;
     }
+
+    cout << endl; // 空行做分隔
+
+    // 輸出九九乘法表
+    cout << "=== 九九乘法表 ===" << endl;
+    int i = 1;
+    while (i <= 9) {
+        int j = 1;
+        while (j <= 9) {
+            cout << i << " * " << j << " = " << i * j << "\t";
+            j += 1;
+        }
+        cout << endl;
+        i += 1;
+    }
+
     return 0;
 }
